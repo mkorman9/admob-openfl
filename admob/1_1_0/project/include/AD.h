270f306742b693273f83abbc4e0e79ed5e500d7a
dynax126 <dynax126@gmail.com>
2014-02-22 13:54:14 +0100

    Initial commit

---
+++ admob/1_1_0/project/include/AD.h
@@ -0,0 +1,13 @@
+#ifndef Ad_IOS
+#define Ad_IOS
+
+namespace admobIOS {
+
+	void initAd(const char *ID, int x, int y, int size, bool testMode);
+	void showAd();
+	void hideAd();
+	void refreshAd();
+
+}
+
+#endif
